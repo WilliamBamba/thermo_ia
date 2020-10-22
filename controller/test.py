b7ee2e2164ffd43316b8f45c6858c6e4a618593c
SH <shajjad.hossain@etu.univ-lyon1.fr>
2020-10-22 21:50:24 +0200

    using python to programme arduino. wrote some tests

--- controller/test.py
+++ controller/test.py
@@ -1,1 +1,16 @@
 import serial
+import time
+
+# this is on mac os. need to verify for other sys
+serial_port = '/dev/cu.usbmodem14101'
+
+
+if __name__ == "__main__":
+    with serial.Serial(port=serial_port) as port:
+        while True:
+            data_wrote = port.write(b'hello\n')
+            # print('data wrote: ', data_wrote)
+            data = port.readline()
+            print(data.decode("utf-8").replace('\r\n', ''))
+            time.sleep(2)
+
