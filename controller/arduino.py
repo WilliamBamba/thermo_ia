1ff41a2a3c8fd493f940e78f6ff8aab2ba8ce63a
SH <shajjad.hossain@etu.univ-lyon1.fr>
2020-11-23 14:01:03 +0100

    using http removed websocket

--- controller/arduino.py
+++ controller/arduino.py
@@ -1,13 +1,17 @@
-import pyfirmata
-import asyncio
-import websockets
+import json
 import time
+import requests
+import pyfirmata
 
 class ArduinoClient:
 
-    def __init__(self, serial_port: str, wc_server_url: str) -> None:
+    """
+        @frequency : in seconds
+    """
+    def __init__(self, serial_port: str, server_url: str, frequency: int = 10) -> None:
         self.serial_port = serial_port
-        self.wc_server_url = wc_server_url
+        self.server_url = server_url
+        self.frequency = frequency
         
         self.arduino_device = None
         self.temp_sensor = None
@@ -19,33 +23,48 @@ class ArduinoClient:
 
     
     async def task(self):
-        while True:
-            try:
-                async with websockets.connect(self.wc_server_url) as wc_client:
-                    while True:
-                        time.sleep(10)
-                        # temperature = self.read_temp_value()
-                        # if temperature is None: continue
-                        # data = {'temperature': temperature}
-                        data = {'temperature': 45}
-                        print(data)
-                        await wc_client.send(str(data))
-            except Exception as e:
-                pass
-
+        pass
+        # loop = asyncio.get_event_loop()
+        # loop.set_debug(True)
+        # loop.run_until_complete(self.task())
+        # loop.close()
+        # while True:
+        #     try:
+        #         async with websockets.connect(self.wc_server_url) as wc_client:
+        #             while True:
+        #                 time.sleep(10)
+        #                 # temperature = self.read_temp_value()
+        #                 # if temperature is None: continue
+        #                 # data = {'temperature': temperature}
+        #                 data = {'temperature': 45}
+        #                 print(data)
+        #                 await wc_client.send(str(data))
+        #     except Exception as e:
+        #         pass
+    
+    def send_sensor_data(self):
+        temperature = self.read_temp_value()
+        if temperature is None: return False
+        payload = {'temperature': temperature}
+        print('sending:', payload)
+        try:
+            requests.post('{}/sensors/'.format(self.server_url), json=payload)
+        except Exception as e:
+            print('error sending data')
+            pass
+    
     def run(self):
-        loop = asyncio.get_event_loop()
-        loop.set_debug(True)
-        loop.run_until_complete(self.task())
-        loop.close()
+        while True:
+            time.sleep(self.frequency)
+            self.send_sensor_data()
+
 
     def connect(self):
-        pass
-        # arduino = pyfirmata.Arduino(self.serial_port)
-        # self.arduino_device = arduino
-        # acquisition = pyfirmata.util.Iterator(arduino)
-        # acquisition.start()
-        # self.temp_sensor = arduino.get_pin('a:0:i')
+        arduino = pyfirmata.Arduino(self.serial_port)
+        self.arduino_device = arduino
+        acquisition = pyfirmata.util.Iterator(arduino)
+        acquisition.start()
+        self.temp_sensor = arduino.get_pin('a:0:i')
         # TODO: ajouter les leds
 
     def read_temp_value(self):
@@ -60,9 +79,9 @@ class ArduinoClient:
 
 
 if __name__ == "__main__":
-    serial_port = '/dev/cu.usbmodem14101'
-    wc_server_url ='ws://localhost:8000/sensors/exchange_data'
+    serial_port = '/dev/cu.usbmodem14201'
+    server_url ='http://localhost:8000'
 
-    client = ArduinoClient(serial_port, wc_server_url)
+    client = ArduinoClient(serial_port, server_url)
 
     client.run()
