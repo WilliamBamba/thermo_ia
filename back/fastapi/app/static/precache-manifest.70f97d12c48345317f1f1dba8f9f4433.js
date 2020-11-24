ac79594ce844d47b91c0fc2dfcc1174ac7d3ff9e
SH <shajjad.hossain@etu.univ-lyon1.fr>
2020-11-24 15:48:38 +0100

    integration front

---
+++ back/fastapi/app/static/precache-manifest.70f97d12c48345317f1f1dba8f9f4433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7499b9a32ebef7b5e3193acdc80e83f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "844b62bc7b73c21bd4b1",
+    "url": "/static/css/main.787e68e4.chunk.css"
+  },
+  {
+    "revision": "7a396566f094a38a3bce",
+    "url": "/static/js/2.ded42dfa.chunk.js"
+  },
+  {
+    "revision": "99740f07e59da1777ca51e4cdac8fee9",
+    "url": "/static/js/2.ded42dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844b62bc7b73c21bd4b1",
+    "url": "/static/js/main.97e5119b.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
